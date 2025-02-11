ac7e8bd01d835f894bbd1b6978a76dcb0d30e0b7
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-11 17:17:06 +0900

    v1.2

--- python_day02.py
+++ python_day02.py
@@ -1,51 +1,17 @@
-# v1.1) for -> while
-# v1.2) while 구문으로 구간 소수를 출력하는 프로그램 작성
-# v1.3) ** 대신 pow 함수
+# v1.2 prime number
 
-# v1.1) for -> while
-# import math
-#
-# def is_prime(num) -> bool:
-#     if num < 2:
-#         return False
-#
-#     i = 2
-#     while i <= int(num ** 0.5):
-#         if num % i == 0:
-#             return False
-#         i += 1
-#
-#     return True
-#
-# n = float(input("Input number : "))
-#
-# sqrt_value = math.sqrt(n)
-# print(f"{n}의 제곱근은 {sqrt_value}입니다.")
+def is_prime(num) -> bool:
 
+numbers = Input("Input number : ").split()
 
-# v1.2) while 구문으로 구간 소수를 출력하는 프로그램 작성
-def is_prime(n):
-    if n < 2:
-        return False
-    i = 2
-    while i * i <= n:
-        if n % i == 0:
-            return False
-        i += 1
-    return True
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-start, end = map(int, input("숫자 두 개 입력: ").split())
+if n1 > n2:
+    n1, n2 = n2, n1
 
-# 시작 숫자가 끝 숫자보다 크면 교체
-if start > end:
-    start, end = end, start
-
-print(f"{start}부터 {end}까지의 소수:")
-num = start
-while num <= end:
-    if is_prime(num):  # 소수 판별 함수 호출
-        print(num, end=" ")
-    num += 1  # 다음 숫자로 이동
-
-
-
+j = n1
+while j <= n2:
+    if is_prime(j):
+        print(j, end=' ')
+    j = j + 1
