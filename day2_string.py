03648be4a2319679b467cfeb4d61a1964e04339d
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-11 13:50:24 +0900

    day2_string

---
+++ day2_string.py
@@ -0,0 +1,18 @@
+# univ = "inha university"
+# print(univ)
+# print(univ[5])
+# univ[5] = 'U' #"imutable"
+# print(univ)
+# # subject = ['python', 'c++', 'linux', 'data structure', 'database']
+# print(subjects)
+# print(subjects[3])
+# subjects[3] = 'data structure & algorithm'
+# print(subjects)
+print(0.1)
+print(1e-1)
+print(0.01)
+print(1e-2)
+print(314.1592)
+print(0.3141592e3)
+print(21000)
+print(21_000) # 언더스코어가 있다고 다른 의미를 갖지는 않음
