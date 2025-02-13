73498b8396a5b73f6fb75c5b3e41904466c6a530
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-13 17:11:55 +0900

    0213ex_2

--- python_day04.py
+++ python_day04.py
@@ -1,31 +1,20 @@
 # Assignment
 # v3.6) 2중 데코레이터 적용, 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
 
-import time
-
-from anaconda_project.internal.conda_api import result
-
-
-@time_decorator
-@description_decorator
-def description(func)
-    def wrapper(*arg)
-        print(func.__name__)
-        print(func.__doc__)
-        r = func(*arg)
-        return result * i
-    return result
-
-
-def time_decoration(func):
-    def wrapper(*arg):
-        s = time.time()
-        r = func(*arg)
-        e = time.time()
-        print(f'실행시간 : {e -s}초')
-        return r
+def log_decorator(func):
+    def wrapper(*args, **kwargs):
+        print(f'Function Name : {func.__name__}')
+        print(f'Function Arguments : {args}')
+        print(f'Function Keywards Arguments : {kwargs}')
+        result = func(*args, **kwargs)
+        return result
     return wrapper
 
-# @time_decorator
-def factorial_repetition(n) -> int:
-    result = 1
+def greet(name, greeting="안녕하세요", age=None):
+    return f"{greeting}, {name}"if age else f"{greeting}, {name}"
+
+print(greet("인하"))
+print(greet(name:"인상", greeting: "안녕"))
+print(greet(name:"James", greeting:"Hello"))
+print(greet(name:"Gonzales", greeting="Hola"))
+print(greet(name:"Nakamura", greeting="Gonizziwa"))
