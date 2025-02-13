4acae2133597dfa69c86d220c0217e75fd8da902
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-13 14:06:59 +0900

    v4.0

--- python_day03.py
+++ python_day03.py
@@ -1,37 +1,33 @@
 import random
-drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
-# print(drinks_foods)
-# print(drinks_foods.pop("고량주"))
-# print(drinks_foods)
 
-#del drinks_foods["위스키"]
-#drinks_foods["사케"] = "광어회"
-japan_drinks_foods = {"사케": "광어회", "위스키": "낙곱새"}
-drinks_foods.update(japan_drinks_foods)
+# (술, 안주) 리스트
+drinks_foods = [
+    ("위스키", "초콜릿"),
+    ("와인", "치즈"),
+    ("소주", "삼겹살"),
+    ("고량주", "양꼬치"),
+    ("사케", "광어회"),
+    ("위스키", "낙곱새")  # 중복된 "위스키" 추가
+]
 
-#drink = input(drinks_foods.keys())
-drinks_foods_keys = list(drinks_foods)
-# print(drinks_foods_keys)
-# #print(drinks_foods_keys.pop(0))
-# print(drinks_foods_keys.remove("위스키"))
-# print(drinks_foods_keys)
-#print(random.choice(drinks_foods_keys))
+# 술 목록 리스트
+drinks_list = [drink[0] for drink in drinks_foods]
 
 while True:
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks_foods_keys[0]}   2) {drinks_foods_keys[1]}   3) {drinks_foods_keys[2]}   4) {drinks_foods_keys[3]}   5) {drinks_foods_keys[4]}   6) 아무거나   7) 종료 : ')
-    if menu == '1':
-        print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]} 입니다')
-    elif menu == '2':
-        print(f'{drinks_foods_keys[1]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[1]]} 입니다')
-    elif menu == '3':
-        print(f'{drinks_foods_keys[2]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[2]]} 입니다')
-    elif menu == '4':
-        print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]} 입니다')
-    elif menu == '5':
-        print(f'{drinks_foods_keys[4]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[4]]} 입니다')
+    menu = input(f'다음 술 중에 고르세요.\n'
+                 f'1) {drinks_list[0]}   2) {drinks_list[1]}   3) {drinks_list[2]}   '
+                 f'4) {drinks_list[3]}   5) {drinks_list[4]}   6) 아무거나   7) 종료 : ')
+
+    if menu in ['1', '2', '3', '4', '5']:
+        index = int(menu) - 1
+        print(f'{drinks_list[index]}에 어울리는 안주는 {drinks_foods[index][1]} 입니다')
+
     elif menu == '6':
-        random_drink = random.choice(drinks_foods_keys)
+        random_index = random.randint(a: 0, len(drinks)-1)
         print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
+
+        # random_drink = random.choice(drinks_foods_keys)
+        # print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]} 입니다')
     elif menu == '7':
         print(f'다음에 또 오세요')
         break
---
+++ python_day04.py
@@ -0,0 +1,6 @@
+z = lamda x: pow(x, 2)
+print(tuple(map(z, ven_numbers)))
+
+ns = [19, 7, -11, -988]
+print(tuple(map(abs, ns)))
+
