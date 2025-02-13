db755fd993afb3e6e8ebec61eea776814cd95e78
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-13 16:39:59 +0900

    0213ex

--- python_day04.py
+++ python_day04.py
@@ -1,13 +1,31 @@
-# SOLID
-# Open Closed Principle : 개방 폐쇄 원칙(확장에는 열려있고 수정에는 닫혀있는 원칙)
+# Assignment
+# v3.6) 2중 데코레이터 적용, 성능측정 데코레이터, 디스크립션 데코레이터를 팩토리얼 함수에 적용하시오.
+
 import time
 
-def factorial_rperirion(n) -> int:
-    result = 1
-    for i in range(2, n+1):
-        result = result * ireturn result
+from anaconda_project.internal.conda_api import result
 
-number = int(input())
-print(f"{number}! = {factorial_rperirion(number)}")
-e = time.time()
-print(e-s)
+
+@time_decorator
+@description_decorator
+def description(func)
+    def wrapper(*arg)
+        print(func.__name__)
+        print(func.__doc__)
+        r = func(*arg)
+        return result * i
+    return result
+
+
+def time_decoration(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e -s}초')
+        return r
+    return wrapper
+
+# @time_decorator
+def factorial_repetition(n) -> int:
+    result = 1
