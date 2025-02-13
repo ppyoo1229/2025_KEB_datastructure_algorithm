741bc28a483b1e3c52a2d2203fcaee0bb05e18cc
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-13 15:25:12 +0900

    v4.2

--- python_day04.py
+++ python_day04.py
@@ -1,6 +1,13 @@
-z = lamda x: pow(x, 2)
-print(tuple(map(z, ven_numbers)))
+# SOLID
+# Open Closed Principle : 개방 폐쇄 원칙(확장에는 열려있고 수정에는 닫혀있는 원칙)
+import time
 
-ns = [19, 7, -11, -988]
-print(tuple(map(abs, ns)))
+def factorial_rperirion(n) -> int:
+    result = 1
+    for i in range(2, n+1):
+        result = result * ireturn result
 
+number = int(input())
+print(f"{number}! = {factorial_rperirion(number)}")
+e = time.time()
+print(e-s)
