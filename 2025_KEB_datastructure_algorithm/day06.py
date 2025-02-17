fabfba1a96efd7d630f63840509c9abdb7a838d3
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-17 11:36:02 +0900

    v6.3 BigO

--- 2025_KEB_datastructure_algorithm/day06.py
+++ 2025_KEB_datastructure_algorithm/day06.py
@@ -1,13 +1,6 @@
-from sympy import factorial
-
-
-def = nCr(n,r) -> int:
-
-numerator = factorial(n)
-denominator = factorial(n-r) * factorial(r)
-return numerator / denominator
-
-if __name__=="__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{n} = {nCr(n, r)}")
+# f(n) = r + n : O(n)
+n = int(input())
+n = 0
+for i in range (n):
+    n = n + 1
+print(n)
