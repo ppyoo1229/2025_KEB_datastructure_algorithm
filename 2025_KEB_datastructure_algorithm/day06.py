b04a1b4bcf6158ced20916d92b02138ea6dc6955
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-17 10:07:45 +0900

    Remove submodule and add as normal folder

---
+++ 2025_KEB_datastructure_algorithm/day06.py
@@ -0,0 +1,10 @@
+def is_even
+    if
+        return True
+    return False
+
+
+a = 10              #0000 1010
+b = 11              #0000 1011
+print(a & b)
+print(a | b)
---
+++ python_day05.py
@@ -0,0 +1,15 @@
+import mathdef my_abs(n) -> int:
+"""
+Return Absolute value of parameter n
+:param n:
+:return: absolute value
+"""
+if n < 0:
+    return-not return not
+# print(mu_abs(-99), my_abs(-19))
+
+#calculrate fivonacci number
+print (tg.tg_abs(-99))
+print (tg.tg_fivonacci_loop(6))
+if__name__ != "==main__"
+        print("tgmath.py 파일을 실행하셨습니다")
