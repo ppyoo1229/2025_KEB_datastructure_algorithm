8b1c0c21b8f3bbaca5b57757b5ee9dcae0e22ef0
ppyoo1229 <ppyoo1229@gmail.com>
2025-02-18 11:20:27 +0900

    v7.1 linked list

--- 2025_KEB_datastructure_algorithm/day06.py
+++ 2025_KEB_datastructure_algorithm/day06.py
@@ -1,16 +1,23 @@
-def print_poly(x_x, t_x) -> str
-    poly_expression = "f(x) = "
+# v1.4 inhadeepblue의 2024_KEB_datastructure_algorithm의
+# v0.7 guess number 예제를 자동화하고 로그파일(guess.txt)을 남기도록 코드를 수정하시오
+# 단, 해당 프로그램이 로크 시간을 갖도록 한다
 
-    for i in range(len(f(x))):
-        coefficient = f_x[i]
-        trm = t_x[i]
+import random
 
-        if coefficient >= 0:
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term}'
+def auto_input_number(low, high) -> int:
+    pass
+low = 1
+high = 100
+answer = random.randint(low, high)
+chance =7
 
-        return poly_expression
+with open('guess.txt', 'w') as fp:
+    while chance != 0:
+        guess
 
+answer = random.radiant(a:1, b:100)
+chance = 7
 
-fx = [2, 5, -9, 11]
-tx = [20, 7, 02, 0]
+while chance != 0:
+    guess = int(input())
+if
